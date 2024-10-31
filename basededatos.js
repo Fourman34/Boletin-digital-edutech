ffbe343a45cd89b60b0ec5c2e2ef1f2ec8d6de21
lautaro escobar <143241630+Fourman34@users.noreply.github.com>
2024-10-30 23:37:44 -0300

    .

--- basededatos.js
+++ basededatos.js
@@ -1,7 +1,5 @@
-// Requiere el paquete de MySQL
 const mysql = require('mysql');
 
-// Crear la conexión
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -9,7 +7,6 @@ const connection = mysql.createConnection({
     database: 'colegio_guevara_boletin1'
 });
 
-// Establecer la conexión
 connection.connect((err) => {
     if (err) {
         console.error('Error conectando a la base de datos:', err.stack);
@@ -18,13 +15,11 @@ connection.connect((err) => {
     console.log('Conexión establecida correctamente');
 });
 
-// Realizar la consulta
 connection.query('SELECT * FROM notas', (err, rows) => {
     if (err) throw err;
     console.log('Los datos solicitados son:');
     console.log(rows);
 
-    // Finalizar la conexión solo después de obtener los resultados
     connection.end((err) => {
         if (err) {
             console.error('Error cerrando la conexión:', err.stack);
