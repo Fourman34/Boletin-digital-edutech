0ffb188525ff687f9bb0ef1ade6f869ac9b0416a
Fourman34 <elautaro137@gmail.com>
2024-10-31 19:39:52 -0300

    commit 81

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send ('hola mundo')
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/:name/:id', (req, res) => {
+    const {name, id} = req.params;
+    res.send(`${name}: ${id}`);  // Usar backticks para interpolación
 });
 
-app.post ('/', (req, res)=> {
-    res.send('peticion POST');
+app.post('/', (req, res) => {
+    const {email, password} = req.body;
+    res.send(`${email}: ${password}`);  // Corregir la sintaxis de res.send
 });
 
-app.listen(3000, ()=>{
+app.put('/', (req, res) => {
+    res.send('petición PUT');
+});
+
+app.delete('/', (req, res) => {
+    res.send('petición DELETE');
+});
+
+app.listen(3000, () => {
     console.log('servidor encendido');
 });
