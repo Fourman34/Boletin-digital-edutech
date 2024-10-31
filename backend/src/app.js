65b4741c13ed333cb1acb392daa6ba7983b4f7e2
lautaro escobar <143241630+Fourman34@users.noreply.github.com>
2024-10-31 00:21:11 -0300

    "Deleted multiple files from the 'pruebas' directory, including HTML, CSS, and image files."

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    response.send ('hola mundo')
+app.get('/', (req, res) => {
+    res.send ('hola mundo')
+});
+
+app.post ('/', (req, res)=> {
+    res.send('peticion POST');
 });
 
 app.listen(3000, ()=>{
