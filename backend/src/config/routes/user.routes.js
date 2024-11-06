1923586d670faaf1d99ac4036acdb814917a93af
Fourman34 <elautaro137@gmail.com>
2024-11-05 22:48:18 -0300

    Update lastUpdateCheck timestamp in nodemon.json

--- backend/src/config/routes/user.routes.js
+++ backend/src/config/routes/user.routes.js
@@ -1,4 +1,4 @@
-// src/config/routes/user.routes.js
+
 const { Router } = require('express');
 const { createUser, readUser, updateUser, deleteUser } = require('../../controllers/user.controller'); // Ajusta la ruta
 const router = Router();
