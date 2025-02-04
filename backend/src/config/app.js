7a02e9b920cc4d2c12f2fbd7345e3d0212cc0eb9
Fourman34 <elautaro137@gmail.com>
2025-02-04 17:56:18 -0300

    `Refactored backend configuration and added frontend routing`

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -1,41 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const userRoutes = require('./routes/user.routes.js');  // Ruta a las rutas de usuarios
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.set('port', 3000);
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.use(express.urlencoded({ extended: true }));
+// Rutas
+app.use('/user', userRoutes);  // Ruta para manejar usuarios
 module.exports = app;
+const path = require('path'); // Importar path
+app.use(express.static(path.join(__dirname, '../../../frontend')));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('C:/Users/lauta/OneDrive/Escritorio/TPI/pagina web/boletin digital Edutech/frontend/index1.html'));
+});
+    // Iniciar el servidor
+    
+app.listen(app.get('port'), () => {
+    console.log(`servidor corriendo en http://localhost:${app.get('port')}`);  // Usa backticks para interpolar la variable
+});
